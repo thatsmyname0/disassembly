6c855e6217ad295dc6093a3976daa06fde267ed5
wrc <208777310@qq.com>
2023-04-16 16:11:01 +0800

    merge: merge lhz's code into mine and add tests for his code.

--- src/instruction.cpp
+++ src/instruction.cpp
@@ -37,3 +37,211 @@ string _sw(string ins)
     int imm=getImmediate(ins);
     return string("sw $")+std::to_string(rt)+","+std::to_string(imm)+"($"+std::to_string(rs)+")";
 }
+
+using std::to_string;
+string StrToInt(string str)
+{
+    int res, i;
+	res = 0;
+    for(i=0; i<str.length(); i++)
+	{
+        res = res * 2 + (str[i] - '0');
+    }
+    string strres;
+    strres = to_string(res);
+    return strres;
+}
+string _add(string ins)
+{
+	string result;
+	result = "add $";
+	
+	string rs, rt, rd, shamt;
+	rs = ins.substr(6, 5);
+    rt = ins.substr(11, 5);
+    rd = ins.substr(16, 5);
+    shamt = ins.substr(21, 5);
+    
+    result = result + StrToInt(rd) + ",$" + StrToInt(rs) + ",$" + StrToInt(rt);
+    return result;
+}
+
+string _addu(string ins)
+{
+	string result;
+	result = "addu $";
+	
+	string rs, rt, rd, shamt;
+	rs = ins.substr(6, 5);
+    rt = ins.substr(11, 5);
+    rd = ins.substr(16, 5);
+    shamt = ins.substr(21, 5);
+    
+    result = result + StrToInt(rd) + ",$" + StrToInt(rs) + ",$" + StrToInt(rt);
+    return result;
+}
+
+string _sub(string ins)
+{
+	string result;
+	result = "sub $";
+	
+	string rs, rt, rd, shamt;
+	rs = ins.substr(6, 5);
+    rt = ins.substr(11, 5);
+    rd = ins.substr(16, 5);
+    shamt = ins.substr(21, 5);
+    
+    result = result + StrToInt(rd) + ",$" + StrToInt(rs) + ",$" + StrToInt(rt);
+    return result;
+}
+
+string _subu(string ins)
+{
+	string result;
+	result = "subu $";
+	
+	string rs, rt, rd, shamt;
+	rs = ins.substr(6, 5);
+    rt = ins.substr(11, 5);
+    rd = ins.substr(16, 5);
+    shamt = ins.substr(21, 5);
+    
+    result = result + StrToInt(rd) + ",$" + StrToInt(rs) + ",$" + StrToInt(rt);
+    return result;
+}
+
+string _and(string ins)
+{
+	string result;
+	result = "and $";
+	
+	string rs, rt, rd, shamt;
+	rs = ins.substr(6, 5);
+    rt = ins.substr(11, 5);
+    rd = ins.substr(16, 5);
+    shamt = ins.substr(21, 5);
+    
+    result = result + StrToInt(rd) + ",$" + StrToInt(rs) + ",$" + StrToInt(rt);
+    return result;
+}
+
+string _or(string ins)
+{
+	string result;
+	result = "or $";
+	
+	string rs, rt, rd, shamt;
+	rs = ins.substr(6, 5);
+    rt = ins.substr(11, 5);
+    rd = ins.substr(16, 5);
+    shamt = ins.substr(21, 5);
+    
+    result = result + StrToInt(rd) + ",$" + StrToInt(rs) + ",$" + StrToInt(rt);
+    return result;
+}
+
+string _xor(string ins)
+{
+	string result;
+	result = "xor $";
+	
+	string rs, rt, rd, shamt;
+	rs = ins.substr(6, 5);
+    rt = ins.substr(11, 5);
+    rd = ins.substr(16, 5);
+    shamt = ins.substr(21, 5);
+    
+    result = result + StrToInt(rd) + ",$" + StrToInt(rs) + ",$" + StrToInt(rt);
+    return result;
+}
+
+string _nor(string ins)
+{
+	string result;
+	result = "nor $";
+	
+	string rs, rt, rd, shamt;
+	rs = ins.substr(6, 5);
+    rt = ins.substr(11, 5);
+    rd = ins.substr(16, 5);
+    shamt = ins.substr(21, 5);
+    
+    result = result + StrToInt(rd) + ",$" + StrToInt(rs) + ",$" + StrToInt(rt);
+    return result;
+}
+
+string _slt(string ins)
+{
+	string result;
+	result = "slt $";
+	
+	string rs, rt, rd, shamt;
+	rs = ins.substr(6, 5);
+    rt = ins.substr(11, 5);
+    rd = ins.substr(16, 5);
+    shamt = ins.substr(21, 5);
+    
+    result = result + StrToInt(rd) + ",$" + StrToInt(rs) + ",$" + StrToInt(rt);
+    return result;
+}
+
+string _sltu(string ins)
+{
+	string result;
+	result = "sltu $";
+	
+	string rs, rt, rd, shamt;
+	rs = ins.substr(6, 5);
+    rt = ins.substr(11, 5);
+    rd = ins.substr(16, 5);
+    shamt = ins.substr(21, 5);
+    
+    result = result + StrToInt(rd) + ",$" + StrToInt(rs) + ",$" + StrToInt(rt);
+    return result;
+}
+
+string _sll(string ins)
+{
+	string result;
+	result = "sll $";
+	
+	string rs, rt, rd, shamt;
+	rs = ins.substr(6, 5);
+    rt = ins.substr(11, 5);
+    rd = ins.substr(16, 5);
+    shamt = ins.substr(21, 5);
+    
+    result = result + StrToInt(rd) + ",$" + StrToInt(rt) + "," + StrToInt(shamt);
+    return result;
+}
+
+string _srl(string ins)
+{
+	string result;
+	result = "srl $";
+	
+	string rs, rt, rd, shamt;
+	rs = ins.substr(6, 5);
+    rt = ins.substr(11, 5);
+    rd = ins.substr(16, 5);
+    shamt = ins.substr(21, 5);
+    
+    result = result + StrToInt(rd) + ",$" + StrToInt(rt) + "," + StrToInt(shamt);
+    return result;
+}
+
+string _jr(string ins)
+{
+	string result;
+	result = "jr $";
+	
+	string rs, rt, rd, shamt;
+	rs = ins.substr(6, 5);
+    rt = ins.substr(11, 5);
+    rd = ins.substr(16, 5);
+    shamt = ins.substr(21, 5);
+    
+    result = result + StrToInt(rs);
+    return result;
+}
--- test/test.cpp
+++ test/test.cpp
@@ -4,7 +4,7 @@
 #include"..\src\utils.h"
 #include"..\src\instruction.h"
 
-TEST_CASE("Testing all I-type(immediate is positive) and J-Type instructions"){
+TEST_CASE("Testing all instructions"){
     MESSAGE("Testing addi");
     CHECK((new MIPS)->convertToAssembly("00100000110000010000000000000001")==string("addi $1,$6,1"));
     MESSAGE("Tesing addiu");
@@ -33,4 +33,29 @@ TEST_CASE("Testing all I-type(immediate is positive) and J-Type instructions"){
     CHECK((new MIPS)->convertToAssembly("00001000011001100010001010000010")==string("j 6693506"));
     MESSAGE("Testing jal");
     CHECK((new MIPS)->convertToAssembly("00001100011001100010001010000010")==string("jal 6693506"));
+
+    MESSAGE("Testing add");
+    CHECK((new MIPS)->convertToAssembly("00000000011000010001000000100000")==string("add $2,$3,$1"));
+    MESSAGE("Testing addu");
+    CHECK((new MIPS)->convertToAssembly("00000000011000010001000000100001")==string("addu $2,$3,$1"));
+    MESSAGE("Testing sub");
+    CHECK((new MIPS)->convertToAssembly("00000000011000010001000000100010")==string("sub $2,$3,$1"));
+    MESSAGE("Testing subu");
+    CHECK((new MIPS)->convertToAssembly("00000000011000010001000000100011")==string("subu $2,$3,$1"));
+    MESSAGE("Testing and");
+    CHECK((new MIPS)->convertToAssembly("00000000011000010001000000100100")==string("and $2,$3,$1"));
+    MESSAGE("Testing or");
+    CHECK((new MIPS)->convertToAssembly("00000000011000010001000000100101")==string("or $2,$3,$1"));
+    MESSAGE("Testing xor");
+    CHECK((new MIPS)->convertToAssembly("00000000011000010001000000100110")==string("xor $2,$3,$1"));
+    MESSAGE("Testing nor");
+    CHECK((new MIPS)->convertToAssembly("00000000011000010001000000100111")==string("nor $2,$3,$1"));
+    MESSAGE("Testing slt");
+    CHECK((new MIPS)->convertToAssembly("00000000011000010001000000101010")==string("slt $2,$3,$1"));
+    MESSAGE("Testing sltu");
+    CHECK((new MIPS)->convertToAssembly("00000000011000010001000000101011")==string("sltu $2,$3,$1"));
+    MESSAGE("Testing sll");
+    CHECK((new MIPS)->convertToAssembly("00000000000000010001000000000000")==string("sll $2,$1,0"));
+    MESSAGE("Testing srl");
+    CHECK((new MIPS)->convertToAssembly("00000000000000010001000000000010")==string("srl $2,$1,0"));
 }
